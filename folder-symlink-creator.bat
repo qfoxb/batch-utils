8ed63d015e3e48caa3336c3ba895fe44986d5800
qfoxb <42038239+qfoxb@users.noreply.github.com>
2020-12-10 21:01:52 -0600

    Update and rename symlink-creator.bat to folder-symlink-creator.bat

---
+++ folder-symlink-creator.bat
@@ -0,0 +1,22 @@
+:: This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+@echo off
+echo This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+echo You are not allowed to distribute modified copies of this software.
+pause
+color 9F
+echo Welcome to qfoxb's Symbolic Folder Link Creator!
+pause
+echo Now, type/paste the folder path the "shortcut" will be creaated.
+set /p shortcutdir=
+IF NOT EXIST %shortcutdir% GOTO SHORTCUTDIREXISTS
+echo Now, type/paste the folder path the shortcut will redirect to.
+set /p linkdir=
+echo Creating a symbolic Folder link.
+mklink /D %shortcutdir% %linkdir%
+echo Symbolic link created!
+echo Thank you for using qfoxb's Symbolic Folder Link Creator.
+pause
+:SHORTCUTDIREXISTS
+echo Uh oh! That directory already exists. Please remove that directory and try again.
+pause
+
